7a9f74941d4a87ca88d1cc32f34650c84ce8b501
Mark Wainwright <wainwrightml@gmail.com>
2023-11-29 08:05:22 +0000

    changed hint borders

--- ws_game/src/game_grid_view.rs
+++ ws_game/src/game_grid_view.rs
@@ -1,6 +1,5 @@
 use crate::prelude::*;
 use bevy_smud::param_usage::ShaderParamUsage;
-use bevy_smud::Frame;
 use maveric::{
     transition::speed::LinearSpeed, widgets::text2d_node::Text2DNode, with_bundle::CanWithBundle,
 };
@@ -39,16 +38,13 @@ impl GridTile {
         if self.selectability.is_selected() {
             2. / 36.
         } else {
-            1. / 36.
+            match self.hint_status {
+                HintStatus::Inadvisable => 0. / 36.,
+                _ => 1. / 36.,
+            }
         }
     }
 }
-
-/*
-TODOS
-    add some sparkles
-
-*/
 
 #[derive(Debug, Clone, Copy, PartialEq, EnumIs)]
 pub enum Selectability {
@@ -203,7 +199,7 @@ impl MavericNode for GridTile {
                 box_with_border_node(
                     tile_size,
                     tile_size,
-                    Vec3::new(0.0, 0.0, crate::z_indices::GRID_BORDER),
+                    Vec3::new(0.0, 0.0, crate::z_indices::GRID_TILE),
                     fill,
                     border_color,
                     0.1,
@@ -212,7 +208,7 @@ impl MavericNode for GridTile {
                 .with_transition_to::<SmudColorLens>(fill, 0.1.into())
                 .with_transition_to::<SmudParamLens<4>>(
                     border_proportion,
-                    border_proportion.into(),
+                    border_proportion.max(1. / 36.).into(),
                 ),
                 &(),
             );
@@ -227,11 +223,10 @@ impl MavericNode for GridTile {
             );
 
             match node.hint_status {
-                HintStatus::ManualHinted | HintStatus::Advisable =>
-                {
-                    let (p0, p1) = if node.hint_status.is_manual_hinted(){
+                HintStatus::ManualHinted => {
+                    let (p0, p1) = if node.hint_status.is_manual_hinted() {
                         (4.0, 3.0)
-                    }else{
+                    } else {
                         (2.0, 2.0)
                     };
                     let seed = node.tile.inner() as f32 * 123.456;
@@ -243,18 +238,9 @@ impl MavericNode for GridTile {
                             sfd: ANYWHERE_SHADER_PATH,
                             fill: SPARKLE_SHADER_PATH,
                             frame_size: 1.0,
-                            params: [
-                                p0,
-                                p1,
-                                seed,
-                                0.0,
-                                0.0,
-                                0.0,
-                                0.0,
-                                0.0,
-                            ],
+                            params: [p0, p1, seed, 0.0, 0.0, 0.0, 0.0, 0.0],
                             sdf_param_usage: ShaderParamUsage::NO_PARAMS,
-                            fill_param_usage: ShaderParamUsage::from_params(&[0,1,2]),
+                            fill_param_usage: ShaderParamUsage::from_params(&[0, 1, 2]),
                         }
                         .with_bundle(Transform {
                             translation: Vec3::Z * 100.0,
@@ -264,12 +250,8 @@ impl MavericNode for GridTile {
                         context,
                     );
                 }
-                 _ => {}
+                _ => {}
             }
-
-
-
-
         })
     }
 
@@ -319,56 +301,3 @@ impl MavericNode for GridLetter {
         });
     }
 }
-
-// #[derive(Debug, PartialEq)]
-// pub struct HintGlows;
-
-// impl MavericNode for HintGlows {
-//     type Context = ViewContext;
-
-//     fn set_components(commands: SetComponentCommands<Self, Self::Context>) {
-//         commands
-//             .ignore_context()
-//             .ignore_node()
-//             .insert(SpatialBundle::default());
-//     }
-
-//     fn set_children<R: MavericRoot>(commands: SetChildrenCommands<Self, Self::Context, R>) {
-//         commands
-//             .ignore_node()
-//             .unordered_children_with_context(|context, commands| {
-//                 let level = context.1.level();
-//                 let solution = context.0.current_solution();
-//                 let manual_hints = context.2.manual_hint_set(level, &solution);
-
-//                 for tile in (manual_hints).iter_true_tiles() {
-//                     let rect = context.3.get_rect(&LayoutGridTile(tile), &());
-
-//                     let translation = rect.centre().extend(crate::z_indices::HINT);
-//                     let tile_size = rect.extents.x;
-
-//                     //let shape = make_rounded_square(tile_size * 0.8, tile_size * 0.1);
-
-//                     let color = palette::MANUAL_HINT_GLOW.convert_color().with_a(0.7);
-
-//                     commands.add_child(
-//                         tile.inner() as u32,
-//                         box_border_node(
-//                             tile_size * 0.8,
-//                             tile_size * 0.8,
-//                             translation,
-//                             color,
-//                             0.1,
-//                             0.1,
-//                         )
-//                         .with_transition_in::<SmudColorLens>(
-//                             color.with_a(0.0),
-//                             color.with_a(0.7),
-//                             Duration::from_secs(1),
-//                         ),
-//                         &(),
-//                     );
-//                 }
-//             });
-//     }
-// }
