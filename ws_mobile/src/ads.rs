b8038efc9b1e6451b0032725aa0fbf13f10645f5
Mark Wainwright <wainwrightml@gmail.com>
2024-03-06 14:07:35 +0000

    removed test features from ads

--- ws_mobile/src/ads.rs
+++ ws_mobile/src/ads.rs
@@ -5,8 +5,8 @@ use nice_bevy_utils::async_event_writer::AsyncEventWriter;
 use ws_common::asynchronous;
 use ws_common::{ads_common, logging, platform_specific, prelude::*};
 
-const MARK_IOS_DEVICE_ID: &str = "d3f1ad44252cdc0f1278cf7347063f07";
-const MARK_ANDROID_DEVICE_ID: &str = "806EEBB5152549F81255DD01CDA931D9";
+// const MARK_IOS_DEVICE_ID: &str = "d3f1ad44252cdc0f1278cf7347063f07";
+// const MARK_ANDROID_DEVICE_ID: &str = "806EEBB5152549F81255DD01CDA931D9";
 
 pub struct AdsPlugin;
 
@@ -87,8 +87,8 @@ async fn reshow_consent_form() -> Result<(), capacitor_bindings::error::Error> {
     let consent_info = Admob::request_consent_info(AdmobConsentRequestOptions {
         debug_geography: AdmobConsentDebugGeography::Disabled,
         test_device_identifiers: vec![
-            MARK_ANDROID_DEVICE_ID.to_string(),
-            MARK_IOS_DEVICE_ID.to_string(),
+            // MARK_ANDROID_DEVICE_ID.to_string(),
+            // MARK_IOS_DEVICE_ID.to_string(),
         ],
         tag_for_under_age_of_consent: false,
     })
@@ -232,10 +232,10 @@ mod mobile_only {
 
     pub async fn try_init_ads_async() -> Result<(), String> {
         Admob::initialize(AdMobInitializationOptions {
-            initialize_for_testing: true,
+            initialize_for_testing: false, //Turn on for testing
             testing_devices: vec![
-                MARK_ANDROID_DEVICE_ID.to_string(),
-                MARK_IOS_DEVICE_ID.to_string(),
+                // MARK_ANDROID_DEVICE_ID.to_string(),
+                // MARK_IOS_DEVICE_ID.to_string(),
             ],
             tag_for_under_age_of_consent: false,
             tag_for_child_directed_treatment: false,
@@ -271,8 +271,8 @@ mod mobile_only {
         let consent_info = Admob::request_consent_info(AdmobConsentRequestOptions {
             debug_geography: AdmobConsentDebugGeography::Disabled,
             test_device_identifiers: vec![
-                MARK_ANDROID_DEVICE_ID.to_string(),
-                MARK_IOS_DEVICE_ID.to_string(),
+                // MARK_ANDROID_DEVICE_ID.to_string(),
+                // MARK_IOS_DEVICE_ID.to_string(),
             ],
             tag_for_under_age_of_consent: false,
         })
