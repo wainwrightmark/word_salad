ba0747d30ff45714e44e77090bbd150239d38bd9
Mark Wainwright <wainwrightml@gmail.com>
2024-03-08 13:53:54 +0000

    added keyboard system to recording feature

--- ws_web/src/main.rs
+++ ws_web/src/main.rs
@@ -1,18 +1,22 @@
 pub mod ads;
 pub mod purchases;
-
 use ads::AdsPlugin;
+
+use itertools::Either;
 use purchases::PurchasesPlugin;
-use ws_common::prelude::*;
-use ws_core::layout::entities::IDEAL_WIDTH;
-use ws_core::layout::entities::IDEAL_HEIGHT;
+use ws_common::{prelude::*, startup::ADDITIONAL_TRACKING};
 
-
-
-#[cfg(feature="recording")]
-use bevy::{core::FrameCount, core_pipeline::bloom::BloomSettings, prelude::*, render::{camera::RenderTarget, render_resource::{Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages}}};
-#[cfg(feature="recording")]
-use bevy_image_export::{ImageExportBundle, ImageExportPlugin, ImageExportSettings, ImageExportSource};
+#[cfg(feature = "recording")]
+use bevy::render::{
+    camera::RenderTarget,
+    render_resource::{
+        Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+    },
+};
+#[cfg(feature = "recording")]
+use bevy_image_export::{
+    ImageExportBundle, ImageExportPlugin, ImageExportSettings, ImageExportSource,
+};
 
 fn main() {
     ws_common::startup::setup_app(add_web_plugins);
@@ -22,27 +26,75 @@ fn add_web_plugins(app: &mut App) {
     app.add_plugins(PurchasesPlugin);
     app.add_plugins(AdsPlugin);
 
-    #[cfg(feature="recording")]
+    #[cfg(feature = "recording")]
     {
         app.add_plugins(ImageExportPlugin::default());
         app.add_systems(Startup, setup_recording);
+        app.add_systems(PreUpdate, handle_keyboard_events);
     }
 }
 
-#[cfg(feature="recording")]
+#[cfg(feature = "recording")]
+fn handle_keyboard_events(
+    mut chosen_state: ResMut<ChosenState>,
+    found_words_state: Res<FoundWordsState>,
+    current_level: Res<CurrentLevel>,
+    daily_challenges: Res<DailyChallenges>,
+    keyboard_events: Res<ButtonInput<KeyCode>>,
+    mut input_state: Local<GridInputState>,
+) {
+    if keyboard_events.just_pressed(KeyCode::Space) {
+        if let Either::Left(level) = current_level.level(&daily_challenges) {
+            const SOLUTION_ORDER: &[usize] = &[1, 2, 0, 3, 4];
+
+            if let Some(so) = SOLUTION_ORDER
+                .into_iter()
+                .filter(|so| {
+                    found_words_state
+                        .word_completions
+                        .get(**so)
+                        .is_some_and(|c| !c.is_complete())
+                })
+                .next()
+            {
+                let word = level.words.get(*so).unwrap();
+                if let Some(solution) =
+                    word.find_solution_with_tiles(&level.grid, found_words_state.unneeded_tiles)
+                {
+                    if let Some(tile) = solution.get(chosen_state.current_solution().len()) {
+                        info!("Selecting {tile} in '{}'", word.text);
+                        input_state.handle_input_start(
+                            &mut chosen_state,
+                            *tile,
+                            &level.grid,
+                            &found_words_state,
+                        );
+
+                        input_state.handle_input_end_no_location();
+                        ADDITIONAL_TRACKING.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                    }
+                }
+            } else {
+                warn!("No more words in solution order");
+            }
+        } else {
+            warn!("No level");
+        }
+    }
+}
+
+#[cfg(feature = "recording")]
 fn setup_recording(
     mut commands: Commands,
     mut images: ResMut<Assets<Image>>,
     mut export_sources: ResMut<Assets<ImageExportSource>>,
 ) {
-
     // Create an output texture.
-
 
     let output_texture_handle = {
         let size = Extent3d {
-            width: IDEAL_WIDTH as u32,
-            height: IDEAL_HEIGHT as u32,
+            width: DEFAULT_WINDOW_WIDTH as u32,
+            height: DEFAULT_WINDOW_HEIGHT as u32,
             ..default()
         };
         let mut export_texture = Image {
@@ -68,7 +120,7 @@ fn setup_recording(
     commands
         .spawn(Camera2dBundle {
             transform: Transform::from_translation(1000.0 * Vec3::Z),
-            camera: Camera{
+            camera: Camera {
                 order: 2,
                 ..Default::default()
             },
